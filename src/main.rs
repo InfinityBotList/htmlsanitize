9cfc876e0b30ad02b46d193d94e2d12ce455eedf
Your Name <cheesycod@outlook.com>
2023-03-21 02:38:41 +0530

    fix

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use axum::{routing::post, Router};
+use axum::{routing::post, Router, http::{self}};
 use pulldown_cmark::{Options, Parser, html::push_html};
 use tower_http::cors::{CorsLayer, Any};
 use std::net::SocketAddr;
@@ -10,7 +10,7 @@ async fn main() {
         CorsLayer::new()
             .allow_origin(Any)
             .allow_methods(Any)
-            .allow_headers(Any),
+            .allow_headers([http::header::CONTENT_TYPE, http::header::ACCEPT, http::header::HeaderName::from_static("x-client")]),
     );
 
     // run it
